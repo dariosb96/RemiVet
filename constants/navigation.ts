4e851873c10f83b49b4103a8526057e2f1f51af2
Dario <xdsb.tstx@gmail.com>
2026-07-28 14:54:32 -0600

    feat(project): create initial project structure

---
+++ constants/navigation.ts
@@ -0,0 +1,24 @@
+import {ROUTES} from "./routes";
+
+export const DASHBOARD_NAVIGATION = [
+    {
+        title: "Dashboard",
+        href: ROUTES.DASHBOARD,
+        icon: "layout-dashboard",
+    },
+    {
+        title: "Citas",
+        href: ROUTES.APPOINTMENTS,
+        icon: "calendar",
+    },
+    {
+        title: "Servicios",
+        href: ROUTES.SERVICES,
+        icon: "briefcase-medical",   
+    },
+    {
+        title: "Configuración",
+        href: ROUTES.SETTINGS,
+        icon: "settings",
+    }
+]
---
+++ constants/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+    HOME: '/',
+    LOGIN: '/login',
+    DASHBOARD: '/dashboard',
+    APPOINTMENTS: '/citas',
+   SERVICES: '/servicios',
+    SETTINGS: '/configuracion',
+    ABOUT: '/about',
+    BOOKING: '/reservar',
+} as const; 
